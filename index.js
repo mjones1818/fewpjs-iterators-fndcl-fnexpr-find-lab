950dc94e780cea27e846780130961ee3aa62af5c
mjones1818 <michael.jones0532@gmail.com>
2021-01-14 16:34:50 -0600

    commit tree f7edde08f08c65c351048e93c7985dd52c8f4e20

--- index.js
+++ index.js
@@ -3,3 +3,9 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+
+superbowlWin = (record) => {
+  let result = record.find( record => record.result === "W" )
+  return !!result ? result.year : undefined
+}
